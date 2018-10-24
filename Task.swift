5001f4d9d64894c1fc52b88795bdf5a97115505d
koyanagibiz <koyanagibiz@gmail.com>
2018-10-24 20:57:11 +0900

    課題提出
    キャンセルボタン追加

---
+++ Task.swift
@@ -0,0 +1,36 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 小柳貴志 on 2018/10/22.
+//  Copyright © 2018年 takashi.koyanagi. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    @objc dynamic var category = ""
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+    
+    
+    
+}
